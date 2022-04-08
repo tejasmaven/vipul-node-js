1798cf4473e0af89a1158fca9bc3b6d9381d1f43
Vipul Jariwala <vipul@maven.one>
2022-04-08 12:03:47 +0530

    cms

--- posts/PostsController.js
+++ posts/PostsController.js
@@ -3,5 +3,5 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 const { Validator } = require('node-input-validator');
 var VerifyToken = require(__root + 'auth/VerifyToken');
-var Posts = require(__root + 'posts/PostsSchema');
+//var Posts = require(__root + 'posts/PostsSchema');
 //var config = require('../config'); // get config file
